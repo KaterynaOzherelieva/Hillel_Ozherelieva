a5788171588b971f831cedc4f8dd64423bf22e36
KaterynaOzherelieva <katenok04091990@gmail.com>
2025-12-26 21:41:15 +0200

    Commit for homework lesson 16_Upd.2

--- homework_lesson_16/work_with_csv/homework_csv_validation.py
+++ homework_lesson_16/work_with_csv/homework_csv_validation.py
@@ -1,31 +1,25 @@
 import csv
 
-unique_rows = []
-seen_rows = set()
+rows = set()
 
 
 with open("rmc.csv", newline="", encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile, delimiter=";")
 
     for row in reader:
-        row_tuple = tuple(row)
+        rows.add(tuple(row))
 
-        if row_tuple not in seen_rows:
-            seen_rows.add(row_tuple)
-            unique_rows.append(row)
 
 
 with open("r-m-c.csv", newline="", encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile, delimiter="\t")
 
     for row in reader:
-        row_tuple = tuple(row)
+        rows.add(tuple(row))
 
-        if row_tuple not in seen_rows:
-            seen_rows.add(row_tuple)
-            unique_rows.append(row)
 
 
 with open("result.csv", "w", encoding="utf-8") as out:
     writer = csv.writer(out)
-    writer.writerows(unique_rows)
+    for row in rows:
+        writer.writerow(row)
